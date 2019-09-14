c916cbeade5cd72c7154de54fcd103ad1ef07256
ZoeLiao <zoejoyuliao@gmail.com>
2019-09-14 17:21:23 +0800

    Add html email

--- demo/demo/settings/prod.py
+++ demo/demo/settings/prod.py
@@ -6,6 +6,8 @@ DEBUG = False
 # 執行 collectatic 後會將項目中的靜態文件收集到此目錄下
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+WEBSITE_URL = 'https://zoeliao.nctu.me'
+
 # Import from local
 #DATABASES = {
 #    'default': {
